72271eee4a7290757d05ce3fecad7a8796e32081
Komal <kumarikomal2164@gmail.com>
2022-10-27 20:31:32 +0530

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -6,89 +6,87 @@ import Navbar from './Component/Navbar';
 import { Link } from 'react-router-dom';
 
 function App(props) {
-    const [books, setBooks] = useState([]);
-    const [currentlyReading, setcurrentlyReading] = useState([]);
-    const [wantToRead, setwantToRead] = useState([]);
-    const [read, setread] = useState([]);
-    const [loading, setloading] = useState(false);
+	const [books, setBooks] = useState([]);
+	const [currentlyReading, setcurrentlyReading] = useState([]);
+	const [wantToRead, setwantToRead] = useState([]);
+	const [read, setread] = useState([]);
+	const [loading, setloading] = useState(false);
 
-    const api = 'https://reactnd-books-api.udacity.com';
-    // Generate a unique token for storing your bookshelf data on the backend server.....
-    let token = localStorage.token;
-    if (!token)
-        token = localStorage.token = Math.random().toString(36).substr(-8);
+	const api = 'https://reactnd-books-api.udacity.com';
+	// Generate a unique token for storing your bookshelf data on the backend server.....
+	let token = localStorage.token;
+	if (!token)
+		token = localStorage.token = Math.random().toString(36).substr(-8);
 
-    const headers = {
-        Accept: 'application/json',
-        Authorization: token,
-    };
-    //This func is for fetching books
-    const getAll = async () => {
-        try {
-            let data = await fetch(`${api}/books`, { headers });
-            let parsedData = await data.json();
-            setBooks(parsedData.books);
-            setloading(false);
-        } catch (error) {
-            console.log('The error is ', error);
-        }
-    };
-    useEffect(() => {
-        setloading(true);
-        getAll();
-    }, []);
-    useEffect(() => {
-        CurrentlyReading();
-        WantToRead();
-        Read();
-    }, [books]);
-    const CurrentlyReading = () => {
-        let array1 = books.filter(
-            (element) => element['shelf'] === 'currentlyReading'
-        );
-        setcurrentlyReading(array1);
-    };
-    const WantToRead = () => {
-        let array2 = books.filter(
-            (element) => element['shelf'] === 'wantToRead'
-        );
-        setwantToRead(array2);
-    };
-    const Read = () => {
-        let array3 = books.filter((element) => element['shelf'] === 'read');
-        setread(array3);
-    };
-    return (
-        <>
-            <Navbar setemail={props.setemail} />
-            <div className="container-fluid" style={{ marginTop: '75px' }}>
-                <Card
-                    heading="Currently Reading"
-                    Reading={currentlyReading}
-                    getAll={getAll}
-                    id={'currentlyReading'}
-                    loading={loading}
-                />
-                <Card
-                    heading="Want to Read"
-                    Reading={wantToRead}
-                    getAll={getAll}
-                    id={'wantToRead'}
-                    loading={loading}
-                />
-                <Card
-                    heading="Read"
-                    Reading={read}
-                    getAll={getAll}
-                    id={'read'}
-                    loading={loading}
-                />
-            </div>
-            <Link to="/search">
-                <div className="btn fixedbutton"></div>
-            </Link>
-        </>
-    );
+	const headers = {
+		Accept: 'application/json',
+		Authorization: token,
+	};
+	//This func is for fetching books
+	const getAll = async () => {
+		try {
+			let data = await fetch(`${api}/books`, { headers });
+			let parsedData = await data.json();
+			setBooks(parsedData.books);
+			setloading(false);
+		} catch (error) {
+			console.log('The error is ', error);
+		}
+	};
+	useEffect(() => {
+		setloading(true);
+		getAll();
+	}, []);
+	useEffect(() => {
+		CurrentlyReading();
+		WantToRead();
+		Read();
+	}, [books]);
+	const CurrentlyReading = () => {
+		let array1 = books.filter(
+			element => element['shelf'] === 'currentlyReading',
+		);
+		setcurrentlyReading(array1);
+	};
+	const WantToRead = () => {
+		let array2 = books.filter(element => element['shelf'] === 'wantToRead');
+		setwantToRead(array2);
+	};
+	const Read = () => {
+		let array3 = books.filter(element => element['shelf'] === 'read');
+		setread(array3);
+	};
+	return (
+		<>
+			<Navbar setemail={props.setemail} />
+			<div className='container-fluid' style={{ marginTop: '75px' }}>
+				<Card
+					heading='Currently Reading'
+					Reading={currentlyReading}
+					getAll={getAll}
+					id={'currentlyReading'}
+					loading={loading}
+				/>
+				<Card
+					heading='Want to Read'
+					Reading={wantToRead}
+					getAll={getAll}
+					id={'wantToRead'}
+					loading={loading}
+				/>
+				<Card
+					heading='Read'
+					Reading={read}
+					getAll={getAll}
+					id={'read'}
+					loading={loading}
+				/>
+			</div>
+			<Link to='/search'>
+				<div className='btn fixedbutton'></div>
+			</Link>
+		</>
+	);
 }
 
 export default App;
